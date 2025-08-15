225e15a12ca78448063e574dcf3f0a017470428d
Jaime <jaimetcf@gmail.com>
2025-08-15 19:13:33 -0300

    Fix to  @ alias paths

--- ui/next.config.js
+++ ui/next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
     domains: ['firebasestorage.googleapis.com'],
   },
   transpilePackages: ['undici', '@firebase/auth'],
+  experimental: {
+    esmExternals: 'loose',
+  },
+  webpack: (config, { isServer }) => {
+    // Ensure proper module resolution
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, './'),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
